50d004221bfed03d3b982006d3023530a14ecfe2
Alex Efros <powerman@powerman.name>
2020-04-23 22:12:36 +0300

    feat: duplicate Tx methods for DB

--- sqlxx.go
+++ sqlxx.go
@@ -21,6 +21,36 @@ func NewDB(db *sqlx.DB) *DB {
 	return &DB{DB: db}
 }
 
+// NamedIn expands slice values in arg returning the modified query string
+// and a new arg list that can be executed by a database.
+func (db *DB) NamedIn(queryIn string, arg interface{}) (query string, args []interface{}, err error) {
+	query, args, err = sqlx.Named(queryIn, arg)
+	if err == nil {
+		query, args, err = sqlx.In(query, args...)
+	}
+	return db.Rebind(query), args, err
+}
+
+// NamedGetContext within a transaction and context.
+// Any named placeholder parameters are replaced with fields from arg.
+func (db *DB) NamedGetContext(ctx Ctx, dest interface{}, query string, arg interface{}) error {
+	query, args, err := db.BindNamed(query, arg)
+	if err == nil {
+		err = db.GetContext(ctx, dest, query, args...)
+	}
+	return err
+}
+
+// NamedSelectContext within a transaction and context.
+// Any named placeholder parameters are replaced with fields from arg.
+func (db *DB) NamedSelectContext(ctx Ctx, dest interface{}, query string, arg interface{}) error {
+	query, args, err := db.BindNamed(query, arg)
+	if err == nil {
+		err = db.SelectContext(ctx, dest, query, args...)
+	}
+	return err
+}
+
 // BeginTxx begins a transaction and returns an *sqlxx.Tx instead of an *sqlx.Tx.
 func (db *DB) BeginTxx(ctx context.Context, opts *sql.TxOptions) (*Tx, error) {
 	tx, err := db.DB.BeginTxx(ctx, opts)
